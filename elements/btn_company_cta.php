ef81f1677eea001f7f1d240be220914f48746053
rander <rander.madureira@gmail.com>
2016-05-11 01:01:38 -0300

    abordagem empresarial

---
+++ elements/btn_company_cta.php
@@ -0,0 +1,3 @@
+<button type="button" class="btn btn-info btn-lg btn-block btn-cta btn-cta-company" data-toggle="modal" data-target=".form-modal-company-lg">
+	Eu quero isso na minha empresa!
+</button>
--- elements/btn_cta.php
+++ elements/btn_cta.php
@@ -1,3 +1,3 @@
-<button type="button" class="btn btn-primary btn-lg btn-block btn-cta" data-toggle="modal" data-target=".form-modal-lg">
+<button type="button" class="btn btn-primary btn-lg btn-block btn-cta btn-cta-usr" data-toggle="modal" data-target=".form-modal-lg">
 	Bote a boca no trombone !
 </button>
